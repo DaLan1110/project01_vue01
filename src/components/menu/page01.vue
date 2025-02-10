dfc1c1b5afc74829aef701adb1902a20b41d1c4f
DaLan <valkyrie777is@gmail.com>
2025-02-10 14:13:46 +0800

    修改05

--- src/components/menu/page01.vue
+++ src/components/menu/page01.vue
@@ -237,7 +237,7 @@ onBeforeUnmount(() => {
   text-decoration: none;
   border: none;
   padding: 5px 20px; /* 調整按鈕內邊距 */
-  border-radius: 10%;
+  border-radius: 50%;
 }
 
 .menu-type-btn-style:hover {
