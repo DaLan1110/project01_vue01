b8d6496eca35d25a9baff59718248aafd877fd7a
DaLan <valkyrie777is@gmail.com>
2025-02-12 09:54:32 +0800

    修改34

--- src/components/personal/page01.vue
+++ src/components/personal/page01.vue
@@ -82,7 +82,7 @@ const isFormValid = computed(() => {
         </div> -->
         <div
           class="card-body d-flex flex-column align-items-center"
-          style="padding: 30px"
+          style="padding-top: 30px"
         >
           <div class="mt-4 w-75">
             <ChangeImg
