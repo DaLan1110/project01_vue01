9c7d76e7d4b5007f63f06e637242a453476fce5a
DaLan <valkyrie777is@gmail.com>
2025-02-17 09:52:34 +0800

    修改10

--- src/components/menu/page01.vue
+++ src/components/menu/page01.vue
@@ -132,24 +132,26 @@ onBeforeUnmount(() => {
         class="container mt-5 d-flex justify-content-center"
         style="margin-bottom: 150px"
       >
-        <div class="row d-flex justify-content-center">
-          <div
-            class="menu-product-card-row"
-            v-for="(item, index) in filteredOrders"
-            :key="index"
-          >
-            <div class="menu-product-card">
-              <div class="card">
-                <img :src="item.product_img" class="card-img-top" alt="" />
-                <div>
-                  <div class="menu-prodect-name-row">
-                    <p class="menu-prodect-name-font">
-                      {{ item.product_name }}
-                    </p>
-                  </div>
+        <div class="d-flex justify-content-center">
+          <div class="menu-product-container">
+            <div
+              class="menu-product-card-row"
+              v-for="(item, index) in filteredOrders"
+              :key="index"
+            >
+              <div class="menu-product-card">
+                <div class="card">
+                  <img :src="item.product_img" class="card-img-top" alt="" />
+                  <div>
+                    <div class="menu-prodect-name-row">
+                      <p class="menu-prodect-name-font">
+                        {{ item.product_name }}
+                      </p>
+                    </div>
 
-                  <!-- <button class="btn mt-5 menu-prodect-btn">商品介紹</button> -->
-                  <MenuBtn :id="item.id" />
+                    <!-- <button class="btn mt-5 menu-prodect-btn">商品介紹</button> -->
+                    <MenuBtn :id="item.id" />
+                  </div>
                 </div>
               </div>
             </div>
@@ -249,9 +251,17 @@ onBeforeUnmount(() => {
   opacity: 0.8;
 }
 
+.menu-product-container {
+  display: flex;
+  flex-wrap: wrap; /* 允許換行 */
+  justify-content: center; /* 讓第一行置中 */
+  max-width: 1000px; /* 限制最大寬度，確保置中效果 */
+}
+
 .menu-product-card-row {
   width: 300px !important;
   height: auto;
+  margin: 10px; /* 設定間距 */
 }
 
 .card-img-top {
